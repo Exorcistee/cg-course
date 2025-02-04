25a2fba281e5f6cf7c63a42929763a2cfbb4fb73
Exorcistee <urazaevkos@gmail.com>
2025-02-04 16:53:28 +0300

    initials

--- lab1/initials/script.js
+++ lab1/initials/script.js
@@ -1,7 +1,11 @@
+const y = 5;
+const width = 100;
+const length = 400;
+let lastX = {x : 0};
+
 window.onload = () => {
     const canvas = document.getElementById("canvas");
     const context = canvas.getContext("2d");
-    let lastX = {x : 0};
 
     drawU(context, lastX);
     drawK(context, lastX);
@@ -9,13 +13,11 @@ window.onload = () => {
 }
 
 function drawU(context, lastX) {
+
     const x = 30;
-    const y = 5;
-    const width = 25;
-    const length = 100;
 
     context.translate(x, y);
-    context.fillStyle = "rgb(252, 1, 1)";
+    context.fillStyle = "rgba(123, 255, 0, 0.88)";
     context.rotate(1.57);
     context.fillRect(0, 0, length, width);
     
@@ -28,7 +30,6 @@ function drawU(context, lastX) {
     context.fillRect(0, 0, length, width);
 
     context.translate(length, 0);
-    context.rotate(0);
     context.fillRect(0, 0, length, width);
     
     context.translate(length, 0);
@@ -42,17 +43,13 @@ function drawU(context, lastX) {
 
 function drawK(context, lastX) {
     const x = 30 + lastX.x;
-    const y = 5;
-    const width = 25;
-    const length = 100;
 
     context.translate(x, y);
-    context.fillStyle = "rgb(252, 1, 1)";
+    context.fillStyle = "rgb(2, 151, 59)";
     context.rotate(1.57);
     context.fillRect(0, 0, length, width);
     
     context.translate(length, 0);
-    context.rotate(0);
     context.fillRect(0, 0, length, width);
 
     context.translate( (length - x + width) / 2 , 0);
@@ -69,58 +66,54 @@ function drawK(context, lastX) {
 
 function drawV(context, lastX) {
     const x = 30 + lastX.x;
-    const y = 5;
-    const width = 25;
-    const length = 100;
+    let startAngle = -(Math.PI / 180) * 90;
+    let endAngle = -(Math.PI / 180) * 270;
+
+    fillStyle = "rgb(0, 222, 252)";
 
     context.translate(x, y);
-    context.fillStyle = "rgb(252, 1, 1)";
+    context.fillStyle = fillStyle;
     context.rotate(1.57);
     context.fillRect(0, 0, length, width);
     
     context.translate(length, 0);
-    context.rotate(0);
     context.fillRect(0, 0, length, width);
 
     context.translate( length - width , 0);
     context.rotate(-1.57);
-    context.fillRect(0, 0, length / 1.3, width);
+    context.fillRect(0, 0, length / 2, width);
 
     context.translate( 0 , -length * 2 + width);
-    context.rotate(0);
     context.fillRect(0, 0, length / 4, width);
 
     context.translate( length / 4, width * 2);
-
-    let startAngle = -(Math.PI / 180) * 90;
-    let endAngle = -(Math.PI / 180) * 270;
     
-    context.strokeStyle = "rgb(255, 0, 0)";
+    context.strokeStyle = fillStyle;
     context.beginPath();
     context.arc(0, 0, length / 2 - width / 2, startAngle, endAngle, false);
     context.lineWidth = width;
     context.stroke();
 
-    context.fillStyle = "rgb(255, 0, 0)";
+    context.fillStyle = fillStyle;
     context.translate( -length / 2, width * 2 - width);
-    context.rotate(0);
     context.fillRect(0, 0, length / 2, width);
 
-    context.fillStyle = "rgb(10, 9, 9)";
-    context.translate( length / 2, width * 2);
-    context.rotate(0);
-    context.fillRect(0, 0, length / 8, width);
+    context.translate( length / 2, width * 2 - width / 4);
 
     startAngle = -(Math.PI / 180) * 90;
     endAngle = -(Math.PI / 180) * 270;
     
     context.translate( length / 4, width);
-    context.strokeStyle = "rgb(255, 0, 0)";
+    context.strokeStyle = fillStyle;
     context.beginPath();
     context.arc(0, 0, length / 2 + - width / 4, startAngle, endAngle, false);
     context.lineWidth = width;
     context.stroke();
     
+    context.fillStyle = fillStyle;
+    context.translate( -length / 2, - length / 2 - width / 4);
+    context.fillRect(0, 0, length / 2, width);
+
     lastX.x = length + 30;
 
     context.setTransform(1,0,0,1,0,0);
