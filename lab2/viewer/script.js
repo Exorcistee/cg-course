7b1a685e041cbfc5f12ba95cd940ad7c5812d9a5
Exorcistee <urazaevkos@gmail.com>
2025-03-03 10:55:39 +0300

    viewer painter

--- lab2/viewer/script.js
+++ lab2/viewer/script.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
+    const button = document.getElementById('open-button');
 
     function openImage() 
-    {
+    {   
         const fileInput = document.getElementById('fileInput');
         fileInput.addEventListener('change', function (e) {
             const file = e.target.files[0];
@@ -11,8 +12,14 @@ document.addEventListener('DOMContentLoaded', function () {
             reader.onload = function (event) {
                 const img = new Image();
                 img.onload = function () {
-                    canvas.width =  img.width;
-                    canvas.height = img.height;
+                    canvas.width = 1280;
+                    canvas.height = 720;
+                    if (!(this.width > canvas.width)){
+                        canvas.width = this.width;   
+                    }
+                    if (!(this.height > canvas.height)){
+                        canvas.height = this.height;   
+                    }
                     context.clearRect(0, 0, canvas.width, canvas.height);
                     context.drawImage(img, 0, 0, canvas.width, canvas.height);
                 };
@@ -23,7 +30,35 @@ document.addEventListener('DOMContentLoaded', function () {
         fileInput.click();
     }
 
+    function useDragAndDrop(element)
+    {
+        var isDragging = false;
+        var offsetX, offsetY;
+    
+        element.addEventListener("mousedown", function(e) {
+            isDragging = true;
+            offsetX = e.clientX - element.getBoundingClientRect().left;
+            offsetY = e.clientY - element.getBoundingClientRect().top;
+        });
+    
+        element.addEventListener("mouseup", function() {
+            isDragging = false;
+        });
+    
+        element.addEventListener("mousemove", function(e) {
+            if (isDragging) {
+                var x = e.clientX - offsetX;
+                var y = e.clientY - offsetY;
+    
+                element.style.left = x + "px";
+                element.style.top = y + "px";
+            }
+        });
+    }
+
     const openButton = document.querySelector('#open-button');
     openButton.addEventListener('click', openImage);
+    useDragAndDrop(canvas);
 
 });
+
